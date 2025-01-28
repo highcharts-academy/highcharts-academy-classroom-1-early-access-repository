b2eadfe7768119fb2613cb37f167f075ea78e91b
mustapha mekhatria <mustapha@highsoft.com>
2025-01-28 12:55:39 +0100

    Update h10.test.cy.js

--- tests/specs/01-highcharts-core/h10.test.cy.js
+++ tests/specs/01-highcharts-core/h10.test.cy.js
@@ -1,64 +1,103 @@
-describe("template spec", () => {
-  const p1 = [500, 200]; // click position
-  const p2 = [300, 250]; // mousemove position
+describe("Line Chart with Custom Cursor and Click Points", () => {
+  const clickPosition = { x: 500, y: 200 }; // Position to trigger click
+  const mouseMovePosition = { x: 300, y: 250 }; // Position to trigger mousemove
 
   beforeEach(() => {
     cy.viewport(800, 600);
-    cy.visit(
-      "../../../exercises/01-highcharts-core/10-click-mousemove-cursor/index.html"
-    );
+    cy.visit("../../../exercises/01-highcharts-core/10-click-mousemove-cursor/index.html");
   });
 
-  it("chart should exist", () => {
+  it("should render the chart", () => {
     cy.get(".highcharts-container").should("be.visible");
   });
 
-  it("the `mouse-circle` should follow the mouse", () => {
-    cy.get(".highcharts-container").trigger("mousemove", ...p2);
-
-    cy.get(".mouse-circle")
-      .should("be.visible")
-      .should("have.attr", "cx", `${p2[0]}`)
-      .should("have.attr", "cy", `${p2[1]}`);
-  });
-
-  it("the `chart-circle` and `chart-text` should be created on click", () => {
-    cy.get(".highcharts-container").trigger("click", ...p1);
-
-    cy.get(".chart-circle")
-      .should("be.visible")
-      .should("have.attr", "cx", `${p1[0]}`)
-      .should("have.attr", "cy", `${p1[1]}`);
-
-    cy.get(".chart-text").should("be.visible");
-  });
-
-  it("position of the `chart-circle` and `chart-text` should be correct after window resize", () => {
-    cy.get(".highcharts-container").trigger("click", ...p1);
+  it("should have a custom cursor that follows the mouse", () => {
+    cy.get(".highcharts-container")
+      .trigger("mousemove", mouseMovePosition.x, mouseMovePosition.y);
 
     cy.window()
-      .its("Highcharts")
-      .then((Highcharts) => {
-        const chart = Highcharts.charts[0];
+      .its("Highcharts.charts[0]")
+      .then((chart) => {
+        const cursor = chart.circleCursor;
+        expect(cursor, "Custom cursor should exist").to.exist;
+
+        // Validate cursor attributes
+        const cursorX = cursor.attr("x");
+        const cursorY = cursor.attr("y");
+
+        expect(cursorX, "Cursor x position").to.equal(mouseMovePosition.x);
+        expect(cursorY, "Cursor y position").to.equal(mouseMovePosition.y);
+      });
+  });
+
+  it("should add a circle and text on chart click", () => {
+    cy.get(".highcharts-container")
+      .trigger("click", clickPosition.x, clickPosition.y);
+
+    cy.window()
+      .its("Highcharts.charts[0]")
+      .then((chart) => {
+        const customPoints = chart.customPoints;
+        expect(customPoints, "Custom points array should not be empty").to.have
+          .length.greaterThan(0);
+
+        const lastPoint = customPoints[customPoints.length - 1];
+        expect(lastPoint.circleGfx.attr("x"), "Circle x position").to.equal(
+          clickPosition.x
+        );
+        expect(lastPoint.circleGfx.attr("y"), "Circle y position").to.equal(
+          clickPosition.y
+        );
+
+        const textContent = lastPoint.textGfx.element.textContent;
+        const x = chart.xAxis[0].toValue(clickPosition.x);
+        const y = chart.yAxis[0].toValue(clickPosition.y);
+
+        expect(textContent).to.contain(`x: ${x.toFixed(2)}`);
+        expect(textContent).to.contain(`y: ${y.toFixed(2)}`);
+      });
+  });
+
+  it("should update custom points correctly on window resize", () => {
+    cy.get(".highcharts-container")
+      .trigger("click", clickPosition.x, clickPosition.y);
+
+    cy.window()
+      .its("Highcharts.charts[0]")
+      .then((chart) => {
         const xAxis = chart.xAxis[0];
         const yAxis = chart.yAxis[0];
 
-        const x = xAxis.toValue(p1[0]);
-        const y = yAxis.toValue(p1[1]);
-
-        cy.get(".chart-text").should(
-          "contain",
-          `x: ${x.toFixed(2)}, y: ${y.toFixed(2)}`
-        );
+        const x = xAxis.toValue(clickPosition.x);
+        const y = yAxis.toValue(clickPosition.y);
 
         cy.viewport(400, 500);
 
-        const pX = xAxis.toPixels(x);
-        const pY = yAxis.toPixels(y);
+        const updatedX = xAxis.toPixels(x);
+        const updatedY = yAxis.toPixels(y);
 
-        cy.get(".chart-circle")
-          .should("have.attr", "cx", `${pX}`)
-          .should("have.attr", "cy", `${pY}`);
+        const customPoints = chart.customPoints;
+        const lastPoint = customPoints[customPoints.length - 1];
+
+        expect(
+          lastPoint.circleGfx.attr("x"),
+          "Updated circle x position after resize"
+        ).to.equal(updatedX);
+
+        expect(
+          lastPoint.circleGfx.attr("y"),
+          "Updated circle y position after resize"
+        ).to.equal(updatedY);
+
+        const updatedTextX = lastPoint.textGfx.attr("x");
+        const updatedTextY = lastPoint.textGfx.attr("y");
+
+        expect(updatedTextX, "Updated text x position after resize").to.equal(
+          updatedX + 10
+        );
+        expect(updatedTextY, "Updated text y position after resize").to.equal(
+          updatedY + 5
+        );
       });
   });
 });
